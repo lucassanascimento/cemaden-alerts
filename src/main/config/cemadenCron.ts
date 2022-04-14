7636331483e41c4f2339958b535782d8e283a5ff
Lucas Sá Nascimento <lucassnascimento20@gmail.com>
2022-04-13 22:08:43 -0300

    feat: active cron

--- src/main/config/cemadenCron.ts
+++ src/main/config/cemadenCron.ts
@@ -1,39 +1,39 @@
-// import { ICemaden } from '@application/interfaces/ICemaden';
-// import { logger } from '@commons/utils/logger';
-// import { IAlertsRepository } from '@domain/Alerts/repositories/IAlertsRepository';
-// import { ICreateAlertsHistoryUseCase } from '@domain/Alerts/usecases';
-// import { ICemadenService } from '@infrastructure/services/CemadenService/ICemadenService';
-// import express from 'express'
-// import cron from 'node-cron'
-// import { container, inject, injectable } from 'tsyringe';
+import { ICemaden } from '@application/interfaces/ICemaden';
+import { logger } from '@commons/utils/logger';
+import { IAlertsRepository } from '@domain/Alerts/repositories/IAlertsRepository';
+import { ICreateAlertsHistoryUseCase } from '@domain/Alerts/usecases';
+import { ICemadenService } from '@infrastructure/services/CemadenService/ICemadenService';
+import express from 'express'
+import cron from 'node-cron'
+import { container, inject, injectable } from 'tsyringe';
 
-// const cemadenCron = express()
+const cemadenCron = express()
 
-// @injectable()
-// class Cemaden implements ICemaden {
-//   constructor(
-//     @inject('CemadenService')
-//     private cemadenService: ICemadenService,
+@injectable()
+class Cemaden implements ICemaden {
+  constructor(
+    @inject('CemadenService')
+    private cemadenService: ICemadenService,
 
-//     @inject('CreateAlertsHistoryUseCase')
-//     private readonly createAlertsHistoryUseCase: ICreateAlertsHistoryUseCase,
-//   ) { }
+    @inject('CreateAlertsHistoryUseCase')
+    private readonly createAlertsHistoryUseCase: ICreateAlertsHistoryUseCase,
+  ) { }
 
-//   handle = async (): Promise<void> => {
-//     try {
-//       const alerts = await this.cemadenService.listAlerts()
-//       await this.createAlertsHistoryUseCase.handle(alerts)
-//       logger.info('Data saved successfully')
-//     } catch (error) {
-//       logger.error(error, 'Error to capture CEMADEN Alerts')
-//     }
-//   }
-// }
+  handle = async (): Promise<void> => {
+    try {
+      const alerts = await this.cemadenService.listAlerts()
+      await this.createAlertsHistoryUseCase.handle(alerts)
+      console.log('Data saved successfully')
+    } catch (error) {
+      logger.error(error, 'Error to capture CEMADEN Alerts')
+    }
+  }
+}
 
-// cron.schedule("*/10 * * * * *", () => {
-//   const alertsController = container.resolve(Cemaden)
-//   alertsController.handle()
-//   logger.info("Runs CRON CEMADEN every 5 seg")
-// });
+cron.schedule("*/10 * * * * *", () => {
+  const alertsController = container.resolve(Cemaden)
+  alertsController.handle()
+  console.log("Runs CRON CEMADEN every 5 seg")
+});
 
-// export default cemadenCron
+export default cemadenCron
